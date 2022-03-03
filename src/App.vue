f24f262bbb8c15c10e8cbb9c91b14e5620227cf0
eh. sh <sh.ehsan74.es@gmail.com>
2022-03-04 00:40:04 +0330

    add LoaderContent component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <div><LoaderFacebook /></div>
-  <div><LoaderDualRing /></div>
-  <div><LoaderCircle /></div>
-  <div><LoaderRoller /></div>
-  <div><LoaderRing /></div>
-  <div><LoaderHeart /></div>
-  <div><LoaderGrid /></div>
-  <div><LoaderEllipsis /></div>
-  <div><LoaderDefault /></div>
-  <div><LoaderSpinner /></div>
-  <div><LoaderRipple /></div>
-  <div><LoaderHourglass /></div>
+  <section>
+    <div><LoaderFacebook /></div>
+    <div><LoaderDualRing /></div>
+    <div><LoaderCircle /></div>
+    <div><LoaderRoller /></div>
+    <div><LoaderRing /></div>
+    <div><LoaderHeart /></div>
+    <div><LoaderGrid /></div>
+    <div><LoaderEllipsis /></div>
+    <div><LoaderDefault /></div>
+    <div><LoaderSpinner /></div>
+    <div><LoaderRipple /></div>
+    <div><LoaderHourglass /></div>
+  </section>
+  <div class="glass" />
+  <LoaderContent :blur="true">
+    <div class="glass" />
+  </LoaderContent>
+  <LoaderContent>
+    <div class="glass" />
+  </LoaderContent>
 </template>
 
 <script lang="ts">
@@ -27,6 +36,7 @@ import LoaderDefault from './components/LoaderDefault.vue';
 import LoaderSpinner from './components/LoaderSpinner.vue';
 import LoaderRipple from './components/LoaderRipple.vue';
 import LoaderHourglass from './components/LoaderHourglass.vue';
+import LoaderContent from './components/LoaderContent.vue';
 
 export default defineComponent({
   name: 'App',
@@ -43,6 +53,7 @@ export default defineComponent({
     LoaderSpinner,
     LoaderRipple,
     LoaderHourglass,
+    LoaderContent,
   },
 });
 </script>
@@ -55,10 +66,22 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background-color: #2c3e50;
 
   & > div {
     margin: 40px 0;
+  }
+
+  section {
+    margin-bottom: 80px;
+    background-color: #2c3e50;
+  }
+
+  .glass {
+    height: 100vh;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url(https://images.unsplash.com/photo-1646316924344-8e162ab28cfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80);
   }
 }
 </style>
---
+++ src/components/LoaderContent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="vb-loaderContent" :class="{ blur }">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoaderContent',
+  props: {
+    blur: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+:root {
+  --vb-loader-content-background-color: #212121;
+  --vb-loader-content-opacity: 0.6;
+  --vb-loader-content-z-index: 999;
+  --vb-loader-content-glass-blur: 20px;
+  --vb-loader-content-glass-background: rgba(255, 255, 255, 0.25);
+}
+.vb-loaderContent {
+  position: relative;
+  z-index: var(--vb-loader-content-z-index);
+
+  &.blur {
+    &::after {
+      background: var(--vb-loader-content-glass-background);
+      backdrop-filter: blur(var(--vb-loader-content-glass-blur));
+      -webkit-backdrop-filter: blur(var(--vb-loader-content-glass-blur));
+      opacity: 1;
+    }
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: var(--vb-loader-content-background-color);
+    opacity: var(--vb-loader-content-opacity);
+  }
+}
+</style>
